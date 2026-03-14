9e79dae831f0360866f10d42b7aba383a665c69a
Sambhrant Maurya <maurya.sam@hotmail.com>
2026-03-15 00:45:40 +0530

    Update spring version in simulator

--- simulator/src/main/java/io/github/throttle/simulator/controller/SimulatorController.java
+++ simulator/src/main/java/io/github/throttle/simulator/controller/SimulatorController.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PreDestroy;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
--- simulator/src/main/java/io/github/throttle/simulator/service/MonitoringService.java
+++ simulator/src/main/java/io/github/throttle/simulator/service/MonitoringService.java
@@ -10,8 +10,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
